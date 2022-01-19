f3dc3da28f7a514c00587e562c764daf52cff5a9
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2022-01-19 22:39:36 +0100

    style: improve styles

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
 import example_basic from "./scripts/leaderboardExamples/example_basic";
 
 const copyNpm = {
-    text: document.getElementById("npm-install"),
-    button: document.getElementById("copy-npm")
+  text: document.getElementById("npm-install"),
+  button: document.getElementById("copy-npm"),
 };
 
 const copyYarn = {
-    text: document.getElementById("yarn-install"),
-    button: document.getElementById("copy-yarn")
+  text: document.getElementById("yarn-install"),
+  button: document.getElementById("copy-yarn"),
 };
 
-const copyCdn = {
-    text: document.getElementById("cdn-install"),
-    button: document.getElementById("copy-cdn")
+const copyCdnJs = {
+  text: document.getElementById("cdn-install-js"),
+  button: document.getElementById("copy-cdn-js"),
 };
 
+const copyCdnCss = {
+  text: document.getElementById("cdn-install-css"),
+  button: document.getElementById("copy-cdn-css"),
+};
 
-const installationCopy = [
-    copyNpm,
-    copyYarn,
-    copyCdn
-]
+const installationCopy = [copyNpm, copyYarn, copyCdnJs, copyCdnCss];
 
-installationCopy.forEach(copyElement => copyElement.button.addEventListener("click", () => {
+installationCopy.forEach((copyElement) =>
+  copyElement.button.addEventListener("click", () => {
     window.navigator.clipboard.writeText(copyElement.text.textContent);
-}))
+  })
+);
 
-example_basic.init()
-
-
-
+example_basic.init();
